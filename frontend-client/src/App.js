54585cf58ade007dd183c31e36c4d7deeff68569
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-12 21:13:58 -0700

    Use access token received from backend to connect to room

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
+const { connect } = require('twilio-video');
 
 function App() {
   const [test, setTest] = useState("");
@@ -9,9 +10,24 @@ function App() {
     axios
       .get("/api")
       .then((response) => {
+        const token = response.data;
         setTest(response.data);
+        return token;
+      })
+      .then(token => {
+        connect(`${token}`, { name: 'cool room'})
+        .then(room => {
+          console.log(`Successfully joined a Room: ${room}`);
+          room.on('participantConnected', participant => {
+            console.log(`A remote Participant connected: ${participant}`);
+          });
+        }, error => {
+          console.error(`Unable to connect to Room: ${error.message}`);
+        });
       })
       .catch((err) => console.log(err));
+
+    
   }, []);
 
   return <h1>test data from backend: {test} </h1>;
