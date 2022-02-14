115d83967e7fec4a86ff803fd9be6111820ec7ea
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-14 15:49:23 -0700

    Create Video component - Stream user video and audio once both webcam and mic are streaming content

---
+++ frontend-client/src/Video.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+export default function Video(props) {
+  const { id, videoFeed, audioFeed } = props;
+  useEffect(() => {
+    // if user's webcam is streaming video
+    if (videoFeed) {
+      document.getElementById(id).appendChild(videoFeed);
+    }
+    // if user's mic is streaming audio
+    if (audioFeed) {
+      document.getElementById(id).appendChild(audioFeed);
+    }
+  }, [id, videoFeed, audioFeed])
+
+  return (
+    <div id={id}>
+    </div>
+  );
+
+}
