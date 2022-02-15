2cbfd2888a91e86158e3d4580d0ce56fa6b7528e
Karen <to.karenngo@gmail.com>
2022-02-14 23:29:27 -0500

    Add OtherVideo comp

---
+++ frontend-client/src/components/OtherVideo.js
@@ -0,0 +1,13 @@
+import SelfVideo from "./SelfVideo";
+
+function OtherVideo(props) {
+  return (
+    <>
+      <div className="other-video">blah</div>
+      <div className="self-video-log">
+          <SelfVideo/>
+      </div>
+    </>
+  );
+}
+export default OtherVideo;
