0e874a938daf656a6180a0e7867742242d10fc57
Karen <to.karenngo@gmail.com>
2022-02-15 15:17:19 -0500

    Add hard coded p text and button comp

--- frontend-client/src/components/AppointmentView/AppointmentListItem.js
+++ frontend-client/src/components/AppointmentView/AppointmentListItem.js
@@ -1,9 +1,23 @@
-import Label from "./Label";
+import Button from "../Button";
+
 function AppointmentListItem (props) {
   return (
-    <>
-      <div className="appt-list-item">Appointment List ITEM</div>
-    </>
+  
+      <div className="appt-list-item">
+        <div className="appt-info-container">
+          <div className="appt-date">
+            <p>DAY</p>
+            <p>Month</p>
+            <p>15th</p>
+          </div>
+          <div className="patient-name-notes">
+            <p>3:00PM - Lisa Jansen</p>
+            <p>Check up examination, shortness of breath.</p>
+          </div>
+        <Button> Join Call</Button>
+        </div>
+      </div>
+    
   );
 }
 export default AppointmentListItem;
