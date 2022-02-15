7d2dc52b63d5ba34bfb8588af138a3000ed458ed
Karen <to.karenngo@gmail.com>
2022-02-15 15:18:10 -0500

    Remove unnecessary fragments and space

--- frontend-client/src/components/AppointmentView/AppointmentView.js
+++ frontend-client/src/components/AppointmentView/AppointmentView.js
@@ -1,10 +1,16 @@
 import NavBar from "../NavBar";
+import AppointmentList from "./AppointmentList";
+import AppointmentHeader from "./AppointmentHeader";
 
 
 function AppointmentView(props) {
   return (
     <div className="appt-view">
       <NavBar/>
+      <div className="appt-section">
+        <AppointmentHeader/>
+        <AppointmentList/>
+      </div>
     </div>
   );
 }
