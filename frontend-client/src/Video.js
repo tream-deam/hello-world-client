720bb2af5eac798371574fe5bc2152b28ba8ace7
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-14 17:46:32 -0700

    Establish live transcription on screen

--- frontend-client/src/Video.js
+++ frontend-client/src/Video.js
@@ -2,6 +2,18 @@ import { useEffect } from "react";
 import useSpeechToText from 'react-hook-speech-to-text';
 
 export default function Video(props) {
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false,
+  });
+
   const { id, videoFeed, audioFeed } = props;
   useEffect(() => {
     // if user's webcam is streaming video
@@ -14,5 +26,26 @@ export default function Video(props) {
     }
   }, [id, videoFeed, audioFeed]);
 
-  return <div id={id}></div>;
+  useEffect(() => {
+    if (results) {
+    console.log(results);
+    }
+  }, [results])
+  
+  if (error) return <p>Web Speech API is not available in this browser :(</p>
+  
+  return (
+    <div id={id}>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Transcribing' : 'Start Transcribing'}
+      </button>
+      <div id="transcription">
+        {interimResult && <li>{interimResult}</li>}
+        {results && <h2>Transcription log:</h2>}
+        {results.map((result) => {
+          return <li key={result.timestamp}> {result.transcript}</li>
+        })}
+      </div>
+    </div>
+  );
 }
