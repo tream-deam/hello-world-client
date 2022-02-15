2e5bbc9a6d2259b6960120cb2adeebc56d65fcfc
Karen <to.karenngo@gmail.com>
2022-02-15 18:16:39 -0500

    Add note about useContext for username

---
+++ frontend-client/src/providers/UsernameProvider.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from 'react';
+
+export const userContext = createContext();
+
+function UsernameProvider(props) {
+  //state logic here
+  //userData being passed below are state variables or functions in the logic that we want to pass to any child components eg. const userData = {user, login, logout, auth}
+
+
+  // return (
+  //   <userContext.Provider value={userData}>
+  //     {props.children}
+  //   </userContext.Provider>
+  // );
+};
+
+export default UsernameProvider;
