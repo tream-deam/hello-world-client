e6f4d528483e3390bc51099e5c29ebbed741f668
Karen <to.karenngo@gmail.com>
2022-02-15 18:17:51 -0500

    Add note about useContext needed for Label comp and Appt header comp to access username

--- frontend-client/src/providers/UsernameProvider.js
+++ frontend-client/src/providers/UsernameProvider.js
@@ -2,6 +2,9 @@ import { createContext, useState } from 'react';
 
 export const userContext = createContext();
 
+
+// We will need this for <Label> for videos as well as <AppointmentHeader>
+
 function UsernameProvider(props) {
   //state logic here
   //userData being passed below are state variables or functions in the logic that we want to pass to any child components eg. const userData = {user, login, logout, auth}
