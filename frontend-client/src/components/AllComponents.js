8a28f261f2d6767cb522f52e459ced7918624754
Karen <to.karenngo@gmail.com>
2022-02-14 23:34:25 -0500

    Split up AllComponents.js into smaller component files

--- frontend-client/src/components/AllComponents.js
+++ frontend-client/src/components/AllComponents.js
@@ -1,13 +1,3 @@
-//import React from "react";
-//import DayListItem from './DayListItem.js';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUserCircle,
-  faCalendarCheck,
-  faBookMedical,
-  faVideo,
-} from "@fortawesome/free-solid-svg-icons";
-
 
 
 export default function AllComponents() {
@@ -15,17 +5,7 @@ export default function AllComponents() {
 
   return (
     <>
-      {/*side nav */}
-      <div className="Callview">
 
-        {/*main video of other person */}
-        <div className="OtherVideo">blah</div>
-        {/*main video of other person */}
-        <div className="SelfVideo-log">
-          <div className="SelfVideo"></div>
-          <div className="convo-log"></div>
-        </div>
-      </div>
     </>
   );
 }
--- frontend-client/src/components/CallView.js
+++ frontend-client/src/components/CallView.js
@@ -5,6 +5,11 @@ function CallView(props) {
   return (
     <div className="call-view">
       <NavBar/>
+      <div className="other-video">blah</div>
+      <div className="self-video-log">
+          <div className="self-video-container"></div>
+          <div className="convo-log"></div>
+      </div>
     </div>
   );
 }
