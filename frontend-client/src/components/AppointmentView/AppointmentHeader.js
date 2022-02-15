c3e64b3794d80393c854c19ac6bd1b013b5ef612
Karen <to.karenngo@gmail.com>
2022-02-15 15:25:03 -0500

    Add hardcore text in appt header

--- frontend-client/src/components/AppointmentView/AppointmentHeader.js
+++ frontend-client/src/components/AppointmentView/AppointmentHeader.js
@@ -1,8 +1,12 @@
 function AppointmentHeader (props) {
 
   return (
+    <>
       <div className="appt-header">Hello, User! </div>
+      {/*STRETCH: conditional rendering of + Appointment button*/}
+      <p>+ Appointment</p>
+    </>
   );
-  
+
 }
 export default AppointmentHeader;
