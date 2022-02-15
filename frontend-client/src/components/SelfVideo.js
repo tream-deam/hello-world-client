ca171b0ee561fb035c1bb8a9ac862a3dbd188a2a
Karen <to.karenngo@gmail.com>
2022-02-14 23:29:56 -0500

    Add SelfVideo component

---
+++ frontend-client/src/components/SelfVideo.js
@@ -0,0 +1,13 @@
+function SelfVideo(props) {
+  return (
+    <div className="call-view">
+      <NavBar/>
+      <div className="other-video">blah</div>
+      <div className="self-video-log">
+          <div className="self-video-container"></div>
+          <div className="convo-log"></div>
+      </div>
+    </div>
+  );
+}
+export default SelfVideo;
