3da4acbdabe63cdf91736dafd1889e9e59167553
Karen <to.karenngo@gmail.com>
2022-02-15 20:43:34 -0500

    Add hard code text and comment slash icon

--- frontend-client/src/components/CallView/ConvoLog.js
+++ frontend-client/src/components/CallView/ConvoLog.js
@@ -1,11 +1,24 @@
 import Label from "./Label";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCommentSlash
+} from "@fortawesome/free-solid-svg-icons";
+
+
 
 function ConvoLog (props) {
   return (
-    <>
-      <div className="convo-log"></div>
-      <Label/>
-    </>
+      <div className="convo-log">
+        <Label/> <h1> Transcription Log:</h1>
+        <p> Comment vous sentez-vous aujourd'hui</p>
+        <p> Je me sens très malade. Je continue d'avoir des migraines.</p>
+     {/*   need to find language icon <FontAwesomeIcon icon="fa-solid fa-language" /> */}
+      <FontAwesomeIcon
+          className="nav-icon"
+          icon={faCommentSlash}
+          size="2x"
+        />
+      </div>
   );
 }
 export default ConvoLog;
