b3c98ba8c27024c54013c341edf547a4980113eb
houseofsam <samxsaid@gmail.com>
2022-02-16 03:09:52 -0500

    Isolate transcription component, refactor, and import to Video component
    can be imported into a more appropriate component later

---
+++ frontend-client/src/Transcription.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import useSpeechToText from "react-hook-speech-to-text";
+
+const Transcription = () => {
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false,
+  });
+
+  // Take the socket we initialize in page load useEffect and store it in state so we can reuse it in different useEffects
+  const [socketState, setSocketState] = useState("");
+  // Store interim results that come from socket listener 'interimListen' into state so that they can be displayed
+  const [stateInterim, setStateInterim] = useState([]);
+  // Store transcription results that come from socket listener 'transcriptionFinish' into state so that they can be displayed
+  const [transcriptionResults, setTranscriptionResults] = useState([]);
+
+  // Initialize socket and listeners to respond to whatever is emitted from the server
+  useEffect(() => {
+    // After initializing socket, save to state to be reused elsewhere
+    const socket = io("/");
+    setSocketState(socket);
+
+    socket.on("connect", () => {
+      console.log("Connected to socket!");
+    });
+
+    // Real time transcription
+    socket.on("interimListen", (msg) => {
+      setStateInterim(msg);
+    });
+
+    // Transcription after a sentence is finished
+    socket.on("transcriptionFinish", (msg) => {
+      setTranscriptionResults((prevTranscriptionResults) => [
+        ...prevTranscriptionResults,
+        msg,
+      ]);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected!");
+    });
+
+    // Ensures we disconnect to avoid memory leaks
+    return () => socket.disconnect();
+  }, []);
+
+  // Whenever a new sentence is transcribed, send it to other client. Only grab the most recent (last) sentence/result
+  useEffect(() => {
+    const socket = socketState;
+    const lastResult = results.slice(-1);
+
+    // This condition prevents transcript from trying to read properties of undefined when there aren't any results
+    if (lastResult[0]) {
+      socket.emit("transcriptionFinish", { msg: lastResult[0].transcript });
+    }
+  }, [socketState, results]);
+
+  // Whenever the interim updates, send data to other client so they can see live transcription as well
+  useEffect(() => {
+    const socket = socketState;
+
+    // This condition prevents the emit method being ran on an undefined object (before socket is initialized)
+    if (socket) {
+      socket.emit("interimListen", { msg: interimResult });
+    }
+  }, [socketState, interimResult]);
+
+  if (error) {
+    return <p> Web Speech API is not available in this browser :( </p>;
+  }
+
+  const remoteTranscriptions = transcriptionResults.map((result, idx) => {
+    return <li key={idx}>{result.msg}</li>;
+  });
+
+  return (
+    <div>
+      {stateInterim.msg}
+      <h1>Remote Transcription Log</h1>
+      {remoteTranscriptions}
+
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? "Stop Transcribing" : "Start Transcribing"}
+      </button>
+
+      <div id="transcription">
+        {interimResult && <li>{interimResult}</li>}
+        {results && <h2>My Transcription log:</h2>}
+        {results.map((result) => {
+          return <li key={result.timestamp}> {result.transcript}</li>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Transcription;
--- frontend-client/src/Video.js
+++ frontend-client/src/Video.js
@@ -1,73 +1,7 @@
-import { useEffect, useState } from "react";
-import useSpeechToText from 'react-hook-speech-to-text';
-import TranscriptionBroadcast from './TranscriptionBroadcast';
-import io from 'socket.io-client';
+import { useEffect } from "react";
+import Transcription from './Transcription';
 
 export default function Video(props) {
-  const {
-    error,
-    interimResult,
-    isRecording,
-    results,
-    startSpeechToText,
-    stopSpeechToText,
-  } = useSpeechToText({
-    continuous: true,
-    useLegacyResults: false,
-  });
-  
-  const [socketState, setSocketState] = useState("");
-  const [transcriptions, setTranscriptions] = useState([]);
-  const [stateInterim, setStateInterim] = useState([]);
-  const [transcriptionResults, setTranscriptionResults] = useState([]);
-
-  useEffect(() => {
-    console.log("testing socket");
-    const socket = io('/');
-    setSocketState(socket);
-
-    socket.on('connect', () => {
-      console.log("connected!");
-    });
-
-    socket.on('transcriptionFinish', (msg) => {
-      console.log(msg);
-      setTranscriptionResults(msg);
-    });
-    
-    socket.on('disconnect', () => {
-      console.log("user disconnected!");
-    });
-    
-    // ensures we disconnect to avoid memory leaks
-    return () => socket.disconnect();
-  }, []);
-
-  useEffect(() => {
-    const socket = socketState;
-
-    const lastResult = results.slice(-1);
-    // console.log(lastResult[0]);
-    if (lastResult[0]) {
-      // socket.emit('transcriptionFinish', {msg: lastResult[0].transcript} )
-    }
-  }, [socketState, results])
-  
-  useEffect(() => {
-    const socket = socketState;
-    setStateInterim(interimResult);
-
-    if (socket) {
-      socket.emit('transcriptionFinish', {msg: interimResult} )
-    }
-    
-  }, [socketState, interimResult])
-
-
-
-  
-  
-
   const { id, videoFeed, audioFeed } = props;
   useEffect(() => {
     // if user's webcam is streaming video
@@ -79,22 +13,10 @@ export default function Video(props) {
       document.getElementById(id).appendChild(audioFeed);
     }
   }, [id, videoFeed, audioFeed]);
-  
-  if (error) return <p>Web Speech API is not available in this browser :(</p>
 
   return (
     <div id={id}>
-       <TranscriptionBroadcast participantTranscription={transcriptionResults}/>
-      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
-        {isRecording ? 'Stop Transcribing' : 'Start Transcribing'}
-      </button>
-      <div id="transcription">
-        {interimResult && <li>{interimResult}</li>}
-        {results && <h2>Transcription log:</h2>}
-        {results.map((result) => {
-          return <li key={result.timestamp}> {result.transcript}</li>
-        })}
-      </div>
+      <Transcription />
     </div>
   );
 }
