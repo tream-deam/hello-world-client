23c742bb1b7ddc6f48db8c410caf5249be490198
Karen <to.karenngo@gmail.com>
2022-02-16 16:10:13 -0500

    Comment out components for netlify

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -1,12 +1,12 @@
 //import Call from "./Call";
 import "./App.css";
 import AllComponents from "./components/AllComponents";
-import CallView from "./components/CallView/CallView";
+//import CallView from "./components/CallView/CallView";
 import "./components/AllComponents.scss";
 import "normalize.css"
 import AppointmentView from "./components/AppointmentView/AppointmentView"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import NavBar from "./components/NavBar";
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import NavBar from "./components/NavBar";
 
 function App() {
   return (
