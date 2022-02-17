dc64ba800b36960e9bc7b385efa6217a754a7b39
houseofsam <samxsaid@gmail.com>
2022-02-17 16:53:23 -0500

    fix bug so that both users can see each other in video chat

--- frontend-client/src/components/CallView/Call.js
+++ frontend-client/src/components/CallView/Call.js
@@ -141,7 +141,7 @@ export default function Call() {
                   const videoTrack = track.attach();
                   setState((prevState) => ({
                     ...prevState,
-                    remoteAudio: videoTrack,
+                    remoteVideo: videoTrack,
                   }));
                 }
               });
