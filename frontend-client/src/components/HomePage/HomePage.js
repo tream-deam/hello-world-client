5f47a3d610beb06b63477b266db2c09628666fbd
Karen <to.karenngo@gmail.com>
2022-02-17 22:37:44 -0500

    Add all html for HomePage comp

--- frontend-client/src/components/HomePage/HomePage.js
+++ frontend-client/src/components/HomePage/HomePage.js
@@ -16,7 +16,7 @@ function HomePage (props) {
               </p>
             </div>
             <p className="brand-description">
-            “Bridging communities around the world [and &amp;] breaking language barriers.”
+            “Bridging communities around the world &amp; breaking language barriers.”
             </p>
           </section>
         <section className="hello-world-options">
@@ -29,21 +29,21 @@ function HomePage (props) {
                 <p className="option-title">Health</p>
                 {/* Heart Icon */}
               </div>
-              <p>Connect with doctors or patients to speak about health concerns. </p>>
+              <p className="desc">Connect with doctors or patients to speak about health concerns. </p>
             </div>
             <div className="learning-container">
               <div className="learning-circle">
                 <p className="option-title">Learning</p>
                 {/* Learning/Brain Icon */}
               </div>
-              <p>Expanding your learning network with others that speak another language. </p>
+              <p className="desc">Expanding your learning network with others that speak another language. </p>
             </div>
             <div className="personal-container">
               <div className="personal-circle">
                 <p className="option-title">Personal</p>
                 {/* 2 people icon */}
               </div>
-              <p>Connect with friends and family around the world. </p>
+              <p className="desc">Connect with friends and family around the world. </p>
             </div>
           </div>
         </section>
