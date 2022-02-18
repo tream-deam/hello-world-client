28ea1485e244b08ef923ed78093dfbfad7bf3087
Karen <to.karenngo@gmail.com>
2022-02-17 20:59:09 -0500

    Add HomePage component to root path

--- frontend-client/src/routers/AppRouter.js
+++ frontend-client/src/routers/AppRouter.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Call from '../components/CallView/Call';
 import AppointmentView from '../components/AppointmentView/AppointmentView';
+import HomePage from '../components/HomePage/HomePage';
 
 const AppRouter = () => (
   <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate replace to="/appointments" />} />
+        {/* <Route path="/" element={<Navigate replace to="/appointments" />} /> */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/appointments" element={<AppointmentView />} />
         <Route path="/video-call" element={<Call />} />
       </Routes>
