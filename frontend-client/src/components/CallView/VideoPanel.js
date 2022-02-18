f5a44089ba5964146d9703254e472c1f6b56d29b
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-17 21:22:04 -0700

    Add translate icon

--- frontend-client/src/components/CallView/VideoPanel.js
+++ frontend-client/src/components/CallView/VideoPanel.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import CallEndIcon from '@mui/icons-material/CallEnd';
+import { CallEnd, Translate } from '@mui/icons-material';
 import {
   faVolumeHigh,
   faMicrophoneSlash,
@@ -13,10 +13,9 @@ export default function VideoPanel() {
     <div id="video-panel">
       <button className="call-button" id="volume"><FontAwesomeIcon icon={faVolumeHigh} /></button>
       <button className="call-button" id="mic-toggle"><FontAwesomeIcon icon={faMicrophoneSlash}/></button>
-      <button className="call-button" id="call-toggle"><CallEndIcon sx={{ fontSize: 40 }}/></button>
+      <button className="call-button" id="call-toggle"><CallEnd sx={{ fontSize: 40 }}/></button>
       <button className="call-button" id="video-toggle"><FontAwesomeIcon icon={faVideo}/></button>
-      <button className="call-button" id="translate"><FontAwesomeIcon icon={faLanguage}/></button>
-      {/* <Button className="call-button" id="call-toggle" defaultContent={<FontAwesomeIcon icon="fa-solid fa-phone-hangup" />}/> */}
+      <button className="call-button" id="translate"><Translate fontSize="large"/></button>
     </div>
   );
 }
