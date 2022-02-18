35eb79e10ed619d587762d6d9e9bcf94d7c6d929
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-18 16:15:44 -0700

    Design fn to get all target languages from Microsoft API

---
+++ frontend-client/src/components/LanguageDropDown/helpers/getTargetLanguages.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export default function getTargetLanguages() {
+  const options = {
+    method: 'GET',
+    url: 'https://api.cognitive.microsofttranslator.com/languages?api-version=3.0'
+  };
+
+  axios
+  .request(options)
+  .then(res => {
+    const translatableLanguages = res.data.translation;
+    const translatableLanguagesWithCodes = [];
+
+    // make array of objects where each element is:
+    // key = language code
+    // value = language name (region if included)
+
+    for (const languageCode in translatableLanguages) {
+      const simplified = {
+        [languageCode]: translatableLanguages[languageCode].name
+      }
+      translatableLanguagesWithCodes.push(simplified);
+    }
+    
+    return translatableLanguagesWithCodes;
+  })
+  .catch(err => console.log(err));
+}
