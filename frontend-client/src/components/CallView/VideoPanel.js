b812c8e65ea689ac5c9ffbf38d234c11f7f4fb0b
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-17 21:15:00 -0700

    Add call button

--- frontend-client/src/components/CallView/VideoPanel.js
+++ frontend-client/src/components/CallView/VideoPanel.js
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import CallEndIcon from '@mui/icons-material/CallEnd';
 import {
   faVolumeHigh,
   faMicrophoneSlash,
@@ -12,10 +13,10 @@ export default function VideoPanel() {
     <div id="video-panel">
       <button className="call-button" id="volume"><FontAwesomeIcon icon={faVolumeHigh} /></button>
       <button className="call-button" id="mic-toggle"><FontAwesomeIcon icon={faMicrophoneSlash}/></button>
+      <button className="call-button" id="call-toggle"><CallEndIcon sx={{ fontSize: 40 }}/></button>
       <button className="call-button" id="video-toggle"><FontAwesomeIcon icon={faVideo}/></button>
       <button className="call-button" id="translate"><FontAwesomeIcon icon={faLanguage}/></button>
-      {/* <button className="call-button" id="call-toggle">{CallEndIcon}</button> 
-      <Button className="call-button" id="call-toggle" defaultContent={<FontAwesomeIcon icon="fa-solid fa-phone-hangup" />}/> */}
+      {/* <Button className="call-button" id="call-toggle" defaultContent={<FontAwesomeIcon icon="fa-solid fa-phone-hangup" />}/> */}
     </div>
   );
 }
