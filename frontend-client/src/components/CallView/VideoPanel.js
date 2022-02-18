5b97385d0481cd8e9e37deed865c61b81decb3d3
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-17 20:23:02 -0700

    Use button elements and fontawesome icons where available

--- frontend-client/src/components/CallView/VideoPanel.js
+++ frontend-client/src/components/CallView/VideoPanel.js
@@ -1,14 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faVolumeHigh,
+  faMicrophoneSlash,
+  faVideo,
+  faLanguage
+} from '@fortawesome/free-solid-svg-icons';
 import Button from "../Button";
 
 export default function VideoPanel() {
   return (
     <div id="video-panel">
-      <Button className="call-button" id="volume" content={<FontAwesomeIcon icon="fa-solid fa-volume-high" />} />
-      <Button className="call-button" id="mic-toggle" default={<FontAwesomeIcon icon="fa-solid fa-microphone-slash" />} />
-      <Button className="call-button" id="call-toggle" content={<FontAwesomeIcon icon="fa-solid fa-phone-hangup" />} />
-      <Button className="call-button" id="video-toggle" content={<FontAwesomeIcon icon="fa-solid fa-video" />} />
-      <Button className="call-button" id="translate" content={<FontAwesomeIcon icon="fa-solid fa-language" />} />
+      <button className="call-button" id="volume"><FontAwesomeIcon icon={faVolumeHigh} /></button>
+      <button className="call-button" id="mic-toggle"><FontAwesomeIcon icon={faMicrophoneSlash}/></button>
+      <button className="call-button" id="video-toggle"><FontAwesomeIcon icon={faVideo}/></button>
+      <button className="call-button" id="translate"><FontAwesomeIcon icon={faLanguage}/></button>
+      {/* <button className="call-button" id="call-toggle">{CallEndIcon}</button> 
+      <Button className="call-button" id="call-toggle" defaultContent={<FontAwesomeIcon icon="fa-solid fa-phone-hangup" />}/> */}
     </div>
   );
 }
