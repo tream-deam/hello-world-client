a2c79ef77d6baea848946f8b7d4640ac3013758a
Karen <to.karenngo@gmail.com>
2022-02-17 22:31:51 -0500

    Wrap Hello Doc in NavLink for AppRouter

--- frontend-client/src/components/NavBar.js
+++ frontend-client/src/components/NavBar.js
@@ -20,12 +20,15 @@ function NavBar() {
   return (
     <div className="nav-container">
         <section className="side-nav">
-          <div id="logo">
-          <h1 id="logo-hello">
-            Hello,  </h1>
-            <h1 id="logo-doc"> 
-            Doc!{" "}</h1>
-          </div>
+          <NavLink to="/" className={(navData) => navData.isActive ? "nav-link is-active" : "nav-link" }>
+            <div id="logo">
+            <h1 id="logo-hello">
+              Hello,  </h1>
+              <h1 id="logo-doc"> 
+              Doc!{" "}</h1>
+            </div>
+          </NavLink>
+     
           <section className="nav-icons">
 
             <NavLink 
@@ -46,14 +49,13 @@ function NavBar() {
               <FontAwesomeIcon className="nav-icon" icon={faVideo} size="2x" />
             </NavLink>
 
-            <NavLink to="/" className={(navData) => navData.isActive ? "nav-link is-active" : "nav-link" }>
+            
               <FontAwesomeIcon
                 className="nav-icon"
                 icon={faBookMedical}
                 size="3x"
               />
-            </NavLink>
-     
+          
           </section>
             <div className="user-profile-nav-container">
               <FontAwesomeIcon
