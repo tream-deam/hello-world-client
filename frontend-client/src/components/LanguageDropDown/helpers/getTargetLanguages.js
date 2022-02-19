2c9ff6c79dd999fe0e86873bfe94a4d741df37b4
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-18 17:11:19 -0700

    Update structure of translatableLanguagesWithCodes to match structure of result of getSourceLanguages

--- frontend-client/src/components/LanguageDropDown/helpers/getTargetLanguages.js
+++ frontend-client/src/components/LanguageDropDown/helpers/getTargetLanguages.js
@@ -2,28 +2,28 @@ import axios from "axios";
 
 export default function getTargetLanguages() {
   const options = {
-    method: 'GET',
-    url: 'https://api.cognitive.microsofttranslator.com/languages?api-version=3.0'
+    method: "GET",
+    url: "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0",
   };
 
   axios
-  .request(options)
-  .then(res => {
-    const translatableLanguages = res.data.translation;
-    const translatableLanguagesWithCodes = [];
+    .request(options)
+    .then((res) => {
+      const translatableLanguages = res.data.translation;
+      const translatableLanguagesWithCodes = [];
 
-    // make array of objects where each element is:
-    // key = language code
-    // value = language name (region if included)
+      // make array of objects where each element is:
+      // key = language code
+      // value = language name (region if included)
 
-    for (const languageCode in translatableLanguages) {
-      const simplified = {
-        [languageCode]: translatableLanguages[languageCode].name
+      for (const languageCode in translatableLanguages) {
+        const languageInfo = {
+          "language-code": languageCode,
+          language: translatableLanguages[languageCode].name,
+        };
+        translatableLanguagesWithCodes.push(languageInfo);
       }
-      translatableLanguagesWithCodes.push(simplified);
-    }
-    
-    return translatableLanguagesWithCodes;
-  })
-  .catch(err => console.log(err));
+      return translatableLanguagesWithCodes;
+    })
+    .catch((err) => console.log(err));
 }
