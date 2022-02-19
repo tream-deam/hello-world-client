f1dd363bca715d9a3da44a5e12aaa50827d024fe
Karen <to.karenngo@gmail.com>
2022-02-18 20:02:35 -0500

    Create HomePage Modal

---
+++ frontend-client/src/components/HomePageModal/HomePageModal.js
@@ -0,0 +1,50 @@
+import './HomePageModal.scss';
+
+const HomePageModal = ({ handleClose, show, children }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+        <div className="modal-header">
+          <div className="close-button" onClick={handleClose}>
+            x
+          </div>
+        </div>
+        <p>Please add your name so your doctor knows you are here.</p>
+                <form autoComplete="off"  onSubmit={(e) => e.preventDefault()}>
+                  <input
+                    className="modal-input"
+                    name="name"
+                    type="text"
+                    placeholder="Your name"
+              /*       value={student} 
+                    onChange={(event) => setStudent(event.target.value)} */
+                    data-testid="username-input"
+                  />
+                </form>
+              <p>Language you speak:</p>
+                <form autoComplete="off"  onSubmit={(e) => e.preventDefault()}>
+                    <input
+                      className="modal-input"
+                      name="language"
+                      type="text"
+                      placeholder="Language you speak"
+                /*       value={student} 
+                      onChange={(event) => setStudent(event.target.value)} */
+                      data-testid="language-input"
+                    />
+                  </form>
+                  <br></br>
+       <footer className="modal-footer">
+          <button className="modal-submit" type="button" onClick={handleClose}>
+            Submit
+          </button>
+        </footer>
+      </section>
+    </div>
+  );
+};
+
+export default HomePageModal
