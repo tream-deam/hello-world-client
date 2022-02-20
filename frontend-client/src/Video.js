3922742f4bba62bf5b8e20360c5e83c5ea439574
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-20 13:20:25 -0700

    Remove self-video node from DOM

--- frontend-client/src/Video.js
+++ frontend-client/src/Video.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useName } from './providers/UsernameProvider';
 
 export default function Video(props) {
-  const { id, videoFeed, audioFeed } = props;
+  const { id, videoFeed, audioFeed, selfDisconnect } = props;
   const name = useName();
   useEffect(() => {
     // if user's webcam is streaming video
@@ -15,6 +15,14 @@ export default function Video(props) {
       document.getElementById(id).appendChild(audioFeed);
     }
   }, [id, videoFeed, audioFeed]);
+
+  useEffect(() => {
+    if (selfDisconnect) {
+      const selfVideoNode = document.getElementsByClassName(`self-video--element`)[0]
+      console.log({selfVideoNode})
+      document.getElementById(id).removeChild(selfVideoNode);
+    }
+  }, [id, selfDisconnect]);
   
 
 
