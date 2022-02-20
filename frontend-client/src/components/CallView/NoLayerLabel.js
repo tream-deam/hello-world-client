b7a4b3e8b10843318ee056682b743d955829a760
Karen <to.karenngo@gmail.com>
2022-02-20 18:43:45 -0500

    Rename NoLayerLabel inside file

--- frontend-client/src/components/CallView/NoLayerLabel.js
+++ frontend-client/src/components/CallView/NoLayerLabel.js
@@ -1,9 +1,9 @@
 //This is the grey/black rounded labels used for users name layered ontop of video as well as 'Translation Log' (we may need to change that name)
 
-function Label (props) {
+function NoLayerLabel (props) {
   return (
     // passing it either the name of self or other OR 'Translation Log' 
     <div className="no-layer-label">{props.text}</div>
   );
 }
-export default Label;
+export default NoLayerLabel;
