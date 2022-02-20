da44423c991caec57798bf4a5045608fe54dec34
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-19 21:57:44 -0700

    Create language state using context API

---
+++ frontend-client/src/providers/LanguageContext.js
@@ -0,0 +1,28 @@
+import React, { useContext, useState } from "react";
+
+const LanguageContext = React.createContext();
+const LanguageUpdateContext = React.createContext();
+
+export function useLanguage() {
+  return useContext(LanguageContext)
+};
+
+export function useLanguageUpdate() {
+  return useContext(LanguageUpdateContext)
+}
+
+export function ThemeProvider({ children }) {
+  const [language, setLanguage] = useState('en-CA');
+
+  function updateLanguage() {
+    setLanguage((prevDarkTheme) => !prevDarkTheme);
+  }
+
+  return (
+    <LanguageContext.Provider value={language}>
+      <LanguageUpdateContext.Provider value={updateLanguage}>
+        {children}
+      </LanguageUpdateContext.Provider>
+    </LanguageContext.Provider>
+  );
+}
