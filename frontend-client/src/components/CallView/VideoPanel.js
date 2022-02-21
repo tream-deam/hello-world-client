19a519b46ba4b44dc64a2a4f549f9a96f56aa5e8
houseofsam <samxsaid@gmail.com>
2022-02-21 01:18:57 -0500

    change name of video buttons class

--- frontend-client/src/components/CallView/VideoPanel.js
+++ frontend-client/src/components/CallView/VideoPanel.js
@@ -13,17 +13,17 @@ export default function VideoPanel(props) {
   const { userDisconnectHandler } = props;
   const coparticipant = useCoparticipant();
 
-  const endCallButtonClass = classNames('call-button', {
-    'show-end-call': !coparticipant
+  const videoButtonsClass = classNames('call-button', {
+    'show-all-video-buttons': !coparticipant
   });
 
   return (
     <>
-      <button className={endCallButtonClass} id="volume"><FontAwesomeIcon icon={faVolumeHigh} /></button>
-      <button className={endCallButtonClass} id="mic-toggle"><FontAwesomeIcon icon={faMicrophoneSlash}/></button>
-      <button className={endCallButtonClass} id="call-toggle"><CallEnd sx={{ fontSize: 40 }} onClick={userDisconnectHandler}/></button>
-      <button className={endCallButtonClass} id="video-toggle"><FontAwesomeIcon icon={faVideo}/></button>
-      <button className={endCallButtonClass} id="translate"><Translate fontSize="large"/></button>
+      <button className={videoButtonsClass} id="volume"><FontAwesomeIcon icon={faVolumeHigh} /></button>
+      <button className={videoButtonsClass} id="mic-toggle"><FontAwesomeIcon icon={faMicrophoneSlash}/></button>
+      <button className={videoButtonsClass} id="call-toggle"><CallEnd sx={{ fontSize: 40 }} onClick={userDisconnectHandler}/></button>
+      <button className={videoButtonsClass} id="video-toggle"><FontAwesomeIcon icon={faVideo}/></button>
+      <button className={videoButtonsClass} id="translate"><Translate fontSize="large"/></button>
     </>
   );
 }
