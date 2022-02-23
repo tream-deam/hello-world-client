9a45ff2ca1c2985cfbc4c9ba266d91bf4dc85e96
Karen <to.karenngo@gmail.com>
2022-02-22 19:53:54 -0500

    Add presentation text samples for AppointmentListItem

--- frontend-client/src/components/AppointmentView/AppointmentListItem.js
+++ frontend-client/src/components/AppointmentView/AppointmentListItem.js
@@ -11,13 +11,36 @@ function AppointmentListItem (props) {
       <div className="appt-list-item-1">
         <div className="appt-info-container">
           <div className="appt-date">
-            <div className="day">SUN</div>
+            <div className="day">THURS</div>
             <div className="month">Feb</div>
-            <div className="date">13th</div>
+            <div className="date">24th</div>
         
           </div>
           <div className="patient-name-notes">
-            <p className="appt-text">3:00PM - Lisa Jansen</p>
+            <p className="appt-text">4:00PM - Lorenço Alvarez Navarrete &amp; Karen Ngo</p>
+            <p className="appt-notes">Check up examination, chronic migraines.</p>
+          </div>
+        </div>
+        <Link to="/video-call">
+          <Button> 
+            {/* must fix this text and icon styling- not sure how things will work with props.children yet when passed to button component */}
+            <div className="join-call-label">
+              <FontAwesomeIcon className="join-video-icon" icon={faVideo} size="1x" />
+              <div className="join-call-text">Join Call</div>
+            </div>
+          </Button>
+        </Link>
+      </div>
+      <div className="appt-list-item">
+        <div className="appt-info-container">
+          <div className="appt-date">
+            <div className="day">FRI</div>
+            <div className="month">Feb</div>
+            <div className="date">25th</div>
+        
+          </div>
+          <div className="patient-name-notes">
+            <p className="appt-text">1:00PM - Sameer Mohammed &amp; Karen Ngo </p>
             <p className="appt-notes">Check up examination, shortness of breath.</p>
           </div>
         </div>
@@ -34,37 +57,14 @@ function AppointmentListItem (props) {
       <div className="appt-list-item">
         <div className="appt-info-container">
           <div className="appt-date">
-            <div className="day">MON</div>
+            <div className="day">SAT</div>
             <div className="month">Feb</div>
-            <div className="date">14th</div>
+            <div className="date">26th</div>
         
           </div>
           <div className="patient-name-notes">
-            <p className="appt-text">3:00PM - Lisa Jansen</p>
-            <p className="appt-notes">Check up examination, shortness of breath.</p>
-          </div>
-        </div>
-        <Link to="/video-call">
-          <Button> 
-            {/* must fix this text and icon styling- not sure how things will work with props.children yet when passed to button component */}
-            <div className="join-call-label">
-              <FontAwesomeIcon className="join-video-icon" icon={faVideo} size="1x" />
-              <div className="join-call-text">Join Call</div>
-            </div>
-          </Button>
-        </Link>
-      </div>
-      <div className="appt-list-item">
-        <div className="appt-info-container">
-          <div className="appt-date">
-            <div className="day">TUES</div>
-            <div className="month">Feb</div>
-            <div className="date">15th</div>
-        
-          </div>
-          <div className="patient-name-notes">
-            <p className="appt-text">3:00PM - Lisa Jansen</p>
-            <p className="appt-notes">Check up examination, shortness of breath.</p>
+            <p className="appt-text">9:00AM - Lisa Jansen &amp; Karen Ngo </p>
+            <p className="appt-notes">Check up examination, stomach pains.</p>
           </div>
         </div>
         <Link to="/video-call">
