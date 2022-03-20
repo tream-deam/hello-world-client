72f6460b5aa72ce9a5b6f20e86218df673088531
houseofsam <samxsaid@gmail.com>
2022-03-20 19:03:30 -0400

    Add hello-doc-backend directory to repo

---
+++ hello-doc-backend/db/seeds/01_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (name, email, password) VALUES ('Alice Watkins', 'alice@mail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Kira Kira', 'kira@mail.com', 'password');
